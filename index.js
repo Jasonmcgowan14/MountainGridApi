d388ea3c169edc50b66f2bc8386aee9ad92fb917
Jason McGowan <jason@welara.com>
2026-02-06 21:49:56 -0700

    remove port 3000

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get("/api/activities/day-counts", async (req, res) => {
  * Server start
  * ============================
  */
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log("====================================");
   console.log(`✅ Server running on http://localhost:${port}`);
