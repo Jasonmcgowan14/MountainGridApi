c80f57380a87045c41ac141269101a9ab4a6dab8
Jason McGowan <jason@welara.com>
2026-02-09 23:15:55 -0700

    render firebase variable paths

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,9 +1,16 @@
-// firebaseAdmin.js
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const json = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
+
+  if (!json) {
+    throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_JSON env var");
+  }
+
+  const serviceAccount = JSON.parse(json);
+
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
